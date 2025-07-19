a9673e2f3ff26ee2db0342e280e588b07e6c6a4a
sebastam97 <sebastianbox97@hotmail.com>
2025-07-18 21:37:21 -0500

    fix:resolve load with params Url

--- app/books/pages/Books.tsx
+++ app/books/pages/Books.tsx
@@ -1,16 +1,33 @@
 "use client";
 import { FormikProvider, useFormik } from "formik";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 import BooksList from "../components/BooksList/BooksList";
 import Search from "../components/Search/Search";
 import { initialsSearchBooks } from "../models/initials/books.initials";
+import { useBooksStore } from "../store/books.store";
 import { BooksContainer, BooksTitle } from "../styles/Books.styles";
 
 function Books() {
+  const searchParams = useSearchParams();
+  const setBooks = useBooksStore((state) => state.setBooks);
+
   const formik = useFormik({
     initialValues: initialsSearchBooks,
-    onSubmit: (values) => {},
+    onSubmit: () => {},
   });
+
+  useEffect(() => {
+    const category = searchParams.get("category");
+    const search = searchParams.get("search");
+
+    if (category) {
+      setBooks(category);
+    } else if (search) {
+      setBooks(search);
+    }
+  }, [searchParams, setBooks]);
 
   return (
     <FormikProvider value={formik}>
